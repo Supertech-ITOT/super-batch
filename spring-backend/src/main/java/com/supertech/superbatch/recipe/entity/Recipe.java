882974694f399343cf845fa449eb00488b9e01ab
Supertech-ITOT <itotsoftware@supertech.co.in>
2026-06-25 10:07:33 +0530

    added

--- spring-backend/src/main/java/com/supertech/superbatch/recipe/entity/Recipe.java
+++ spring-backend/src/main/java/com/supertech/superbatch/recipe/entity/Recipe.java
@@ -2,7 +2,6 @@ package com.supertech.superbatch.recipe.entity;
 
 import java.time.LocalDateTime;
 
-import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.supertech.superbatch.common.enums.UomType;
