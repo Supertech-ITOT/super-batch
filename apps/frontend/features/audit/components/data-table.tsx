d89e75d0897f68822ab040600773861922d04109
SupertechITOT <itotsoftware@supertech.co.in>
2026-07-27 11:24:23 +0530

    added

--- apps/frontend/features/audit/components/data-table.tsx
+++ apps/frontend/features/audit/components/data-table.tsx
@@ -1,19 +1,37 @@
 "use client";
 
-import { ColumnDef, ColumnFiltersState, SortingState, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable, } from "@tanstack/react-table";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, } from "@/common/components/ui/table";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/common/components/ui/table";
 import { Button } from "@/common/components/ui/button";
 import { Input } from "@/common/components/ui/input";
 import { useState } from "react";
-import { Plus } from "lucide-react";
-
 
 interface DataTableProps<TData extends { id: number }, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
 }
 
-const DataTable = <TData extends { id: number }, TValue>({ columns, data }: DataTableProps<TData, TValue>) => {
+const DataTable = <TData extends { id: number }, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) => {
   const [sorting, setSorting] = useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const table = useReactTable({
@@ -34,7 +52,7 @@ const DataTable = <TData extends { id: number }, TValue>({ columns, data }: Data
     <div className="flex flex-col h-full">
       <div className="flex items-center justify-between pb-2">
         <Input
-          placeholder="Filter recipes..."
+          placeholder="Filter audits..."
           value={(table.getColumn("action")?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
             table.getColumn("action")?.setFilterValue(event.target.value)
@@ -49,8 +67,16 @@ const DataTable = <TData extends { id: number }, TValue>({ columns, data }: Data
               <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map((header) => {
                   return (
-                    <TableHead key={header.id} className="font-semibold border-r border-muted-foreground/20 last:border-r-0 text-center">
-                      {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext(),)}
+                    <TableHead
+                      key={header.id}
+                      className="font-semibold border-r border-muted-foreground/20 last:border-r-0 text-center"
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
                     </TableHead>
                   );
                 })}
@@ -71,7 +97,10 @@ const DataTable = <TData extends { id: number }, TValue>({ columns, data }: Data
                         key={cell.id}
                         className={`border-r border-muted-foreground/20 ${cell.column.id === "recipe" ? "text-left" : "text-center"}`}
                       >
-                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
                       </TableCell>
                     ))}
                   </TableRow>
@@ -133,10 +162,20 @@ const DataTable = <TData extends { id: number }, TValue>({ columns, data }: Data
         </div>
 
         <div className="space-x-2">
-          <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()} >
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
             Previous
           </Button>
-          <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()} >
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
             Next
           </Button>
         </div>
